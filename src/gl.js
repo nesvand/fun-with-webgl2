f951bf63944e993ed0361488adcfdf162406de2c
Andrew Nesvadba <nesvand@gmail.com>
2017-02-18 13:48:15 +1000

    feat: base implementation

--- src/gl.js
+++ src/gl.js
@@ -1,1 +1,36 @@
-export default function GLInstance() { }
+export default function GLInstance(canvasID) {
+  const canvas = document.getElementById(canvasID);
+  const gl = canvas.getContext('webgl');
+
+  if (!gl) {
+    console.error('WebGL context is not available.'); // eslint-disable-line
+    return null;
+  }
+
+  // Setup GL - Set all the default configurations we need
+  gl.clearColor(1.0, 1.0, 1.0, 1.0); // Set clear colour
+
+  // Methods
+  gl.fClear = function () {
+    this.clear(this.COLOR_BUFFER_BIT | this.DEPTH_BUFFER_BIT);
+    return this;
+  }
+
+  // Setters/Getters
+
+  // Set the size of the canvas html element and the rendering view port
+  gl.fSetSize = function (w, h) {
+    this.canvas.style.width = w + 'px';
+    this.canvas.style.height = h + 'px';
+    this.canvas.width = w;
+    this.canvas.height = h;
+
+    // when updating the canvas size we must reset the viewport of the canvas
+    // else the reoslution webgl renders at will not change
+    this.viewport(0, 0, w, h);
+
+    return this;
+  }
+
+  return gl;
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
+import './app.css';
+import 'babel-polyfill';
 import GLInstance from './gl';
 
-import './app.css';
+let gl;
+
+window.addEventListener('load', () => {
+  // Get our extended GL Context Object
+  gl = GLInstance('glcanvas').fSetSize(500, 500).fClear();
+});
