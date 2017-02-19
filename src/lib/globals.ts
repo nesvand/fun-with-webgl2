198effa94301443a415b1f2e3e55d3edcc0f5418
Andrew Nesvadba <nesvand@gmail.com>
2017-02-19 14:28:00 +1000

    feat: add globals

---
+++ src/lib/globals.ts
@@ -0,0 +1,15 @@
+const ATTR_POSITION_NAME = 'a_position';
+const ATTR_POSITION_LOC = 0;
+const ATTR_NORMAL_NAME = 'a_norm';
+const ATTR_NORMAL_LOC = 1;
+const ATTR_UV_NAME = 'a_uv';
+const ATTR_UV_LOC = 2;
+
+export {
+  ATTR_NORMAL_LOC,
+  ATTR_NORMAL_NAME,
+  ATTR_POSITION_LOC,
+  ATTR_POSITION_NAME,
+  ATTR_UV_LOC,
+  ATTR_UV_NAME
+}
