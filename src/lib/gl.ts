e2f64495fdbfa49364dcbca983acc0074f95b4e5
Andrew Nesvadba <nesvand@gmail.com>
2017-02-24 22:19:35 +1000

    chore: fix extended gl context

--- src/lib/gl.ts
+++ src/lib/gl.ts
@@ -4,44 +4,30 @@ import {
   ATTR_UV_LOC,
 } from './globals';
 
-export default function GLInstance (canvasID: string) {
+export default function GLInstance(canvasID: string) {
   const canvas = <HTMLCanvasElement>document.getElementById(canvasID);
   const gl = <ExtendedWebGLContext>canvas.getContext('webgl2');
 
   if (!gl) {
-    console.error('WebGL context is not available.'); // eslint-disable-line
-    return null;
+    throw Error('WebGL context is not available.');
   }
 
   gl.mMeshCache = {}; // Cache all the mesh structs, easy to unload buffers if they all exist in one place
 
-  // Setup GL - Set all the default configurations we need
+  //---------------------
+  // Setup GL with default configs
+  gl.cullFace(gl.BACK);
+  gl.frontFace(gl.CCW);
+  gl.enable(gl.DEPTH_TEST);
+  gl.enable(gl.CULL_FACE);
+  gl.depthFunc(gl.LEQUAL);
+  gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
   gl.clearColor(1, 1, 1, 1); // Set clear colour
 
   // Methods
   gl.fClear = function () {
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
     return gl;
-  }
-
-  // Setters/Getters
-
-  // Set the size of the canvas html element and the rendering view port
-  gl.fSetSize = function (w, h) {
-    gl.canvas.style.width = w + 'px';
-    gl.canvas.style.height = h + 'px';
-    gl.canvas.width = w;
-    gl.canvas.height = h;
-
-    // when updating the canvas size we must reset the viewport of the canvas
-    // else the reoslution webgl renders at will not change
-    gl.viewport(0, 0, w, h);
-
-    return gl;
-  }
-
-  gl.fFitScreen = function (wp: number, hp: number) {
-    return this.fSetSize(window.innerWidth * (wp || 1), window.innerHeight * (hp || 1));
   }
 
   gl.fCreateArrayBuffer = function (floatArray, isStatic = true) {
@@ -94,19 +80,42 @@ export default function GLInstance (canvasID: string) {
       meshVAO.bufIndex = gl.createBuffer();
       meshVAO.indexCount = arrayIndex.length;
 
-      gl.bindBuffer(gl.ARRAY_BUFFER, meshVAO.bufIndex);
-      gl.bufferData(gl.ARRAY_BUFFER, new Uint16Array(arrayIndex), gl.STATIC_DRAW);
-      gl.vertexAttribPointer(ATTR_UV_LOC, 2, gl.FLOAT, false, 0, 0);
+      gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, meshVAO.bufIndex);
+      gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(arrayIndex), gl.STATIC_DRAW);
     }
 
-    // Clean updating
+    // Clean up
     gl.bindVertexArray(null); // Always unbind a VAO when you are done using one
     gl.bindBuffer(gl.ARRAY_BUFFER, null); // Unbind any buffers that may be set
+
+    if (arrayIndex !== null && arrayIndex !== undefined) {
+      gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
+    }
 
     gl.mMeshCache[name] = meshVAO;
 
 
     return meshVAO;
+  }
+
+  // Setters/Getters
+
+  // Set the size of the canvas html element and the rendering view port
+  gl.fSetSize = function (w, h) {
+    gl.canvas.style.width = w + 'px';
+    gl.canvas.style.height = h + 'px';
+    gl.canvas.width = w;
+    gl.canvas.height = h;
+
+    // when updating the canvas size we must reset the viewport of the canvas
+    // else the reoslution webgl renders at will not change
+    gl.viewport(0, 0, w, h);
+
+    return gl;
+  }
+
+  gl.fFitScreen = function (wp: number, hp: number) {
+    return gl.fSetSize(window.innerWidth * (wp || 1), window.innerHeight * (hp || 1));
   }
 
   return gl;
