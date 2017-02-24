abe5492dffb9d06057deee42532b44f210de7053
Andrew Nesvadba <nesvand@gmail.com>
2017-02-24 22:04:54 +1000

    chore: test quads

--- src/index.ts
+++ src/index.ts
@@ -10,9 +10,11 @@ import { Camera, CameraController, } from './lib/Camera';
 
 // Shaders
 import GridAxisShader from './shaders/GridAxisShader';
+import vShader from './shaders/vShader.glsl';
+import fShader from './shaders/fShader.glsl';
 
 // Models
-import { GridAxis, } from './lib/Primatives';
+import { GridAxis, Quad, } from './lib/Primatives';
 
 window.addEventListener('load', () => {
   // Global Context
@@ -29,6 +31,10 @@ window.addEventListener('load', () => {
   let gridShader: Shader;
   let gridModel: Model;
 
+  let testShader: Shader;
+  let gModel: Model;
+  let gModel2: Model;
+
   gl = GLInstance('glcanvas');
 
   if (gl) {
@@ -39,22 +45,41 @@ window.addEventListener('load', () => {
     gCamera.transform.position.set(0, 1, 3);
     gCameraCtrl = new CameraController(gl, gCamera);
 
+    // Setup Grid
     gridShader = new GridAxisShader(gl, gCamera.projectionMatrix);
-
     gridModel = GridAxis.createModel(gl, true);
+
+    testShader = new TestShader(gl, gCamera.projectionMatrix);
+    gModel = Quad.createModel(gl);
+    gModel.setPosition(0, 1, 0).setScale(0.2, 0.2, 0.2);
+
+    gModel2 = new Model(gl.mMeshCache['Quad']);
 
     new RenderLoop(onRender).start();
   }
 
-  function onRender () {
-    gCamera.updateViewMatrix();
-
+  function onRender() {
     if (gl) {
+      gCamera.updateViewMatrix();
       gl.fClear();
 
       gridShader.activate()
         .setCameraMatrix(gCamera.viewMatrix)
         .renderModel(gridModel.preRender());
+
+      testShader.activate()
+        .setCameraMatrix(gCamera.viewMatrix)
+        .renderModel(gModel.preRender())
+        .renderModel(gModel2.preRender());
     }
   }
 });
+
+class TestShader extends Shader {
+  constructor(gl: ExtendedWebGLContext, projectionMatrix: MixedFloat32Array) {
+    super(gl, vShader, fShader);
+
+    this.setPerspective(projectionMatrix);
+    gl.useProgram(null);
+  }
+}
