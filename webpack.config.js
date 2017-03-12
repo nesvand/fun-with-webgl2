be5fc1f5c93ea67219be91818eade9673ffffdd2
Andrew Nesvadba <nesvand@gmail.com>
2017-03-12 19:58:24 +1000

    chore: move to docs

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,7 @@ module.exports = {
     app: path.resolve(__dirname, 'src', 'index.ts'),
   },
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'docs'),
     filename: '[name]-[hash].js',
   },
   plugins: [
